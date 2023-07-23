16f5ce16028dafadb6286430dae6de7492cdbfa5
Wyatt McMurry <wyattmcmurry1@gmail.com>
2023-07-23 15:14:23 -0400

    node ts setup with docker

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'wurld' });
+});
+
+const port = Number(process.env.PORT || 8080);
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
